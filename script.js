ce34753165ee2d0770effecb6b687ce2551e2374
Vano <knap1428@gmail.com>
2025-06-12 18:14:28 +0300

    home_work js_code

---
+++ script.js
@@ -0,0 +1,60 @@
+let a = 10;
+alert(a);
+a = 20;
+alert(a);
+// задание 1
+
+const firstIphone = 2007;
+alert(firstIphone);
+//задание 2
+
+const creatorJavaScript = "Brendan Eich!!!";
+alert(creatorJavaScript);
+// задание 3
+
+let b = 10;
+let c = 2;
+let sum = b + c;
+alert("Сумма: " + sum);
+let difference = b - c;
+alert("Разность: " + difference);
+let product = b * c;
+alert("Произведение: " + product);
+let quotient = b / c;
+alert("Частное: " + quotient);
+// задание 4
+
+let result = 2 ** 5;
+alert(result);
+// задание 5
+
+let d = 9;
+let e = 2;
+let remainder = d % e;
+alert(remainder);
+// задание 6
+
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num++;
+num--;
+alert(num);
+// задание 7
+
+let age = prompt("Сколько Вам лет?");
+alert("Ваш возраст: " + age);
+// задание 8
+
+const user = {
+  name: "Ivan",
+  age: 37,
+  isAdmin: false,
+};
+// задание 9
+
+let userName = prompt("Как Вас зовут?");
+alert(`Привет, ${userName}!`);
+// задание 10
