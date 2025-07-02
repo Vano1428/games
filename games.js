7fbe2ccbe740e9a9931eb51b9f900715d2022a37
Vano <knap1428@gmail.com>
2025-07-02 21:43:51 +0300

    ++

---
+++ games.js
@@ -0,0 +1,17 @@
+function startGame1() {
+  let randomNumber = Math.floor(Math.random() * 100) + 1;
+  while (true) {
+    let guessNumber = prompt("Угадай число от 1 до 100");
+    guessNumber = Number(guessNumber);
+    if (guessNumber === randomNumber) {
+      alert("Поздравляю, ты угадал число!");
+      break;
+    } else if (guessNumber > randomNumber) {
+      alert("Твое число больше загаданного, попробуй еще!");
+    } else {
+      alert("Твое число меньше загаданного, попробуй еще!");
+    }
+  }
+}
+
+console.log(randomNumber);
