1cda64e1e056f971648e6776bcfed1b99e4b84ed
Vano <knap1428@gmail.com>
2025-07-15 16:43:32 +0300

    ^^^

--- script6.js
+++ script6.js
@@ -18,3 +18,89 @@ let roundNum = Math.round(num);
 console.log("Округление до меньшего целого числа:", floorNum);
 console.log("Округление до большего целого числа:", ceilNum);
 console.log("Округление до ближайшего целого числа:", roundNum); // задание 3
+
+const numbers = [52, 53, 49, 77, 21, 32];
+
+const min = Math.min(...numbers);
+const max = Math.max(...numbers);
+
+console.log("Минимальное значение:", min);
+console.log("Максимальное значение:", max); // задание 4
+
+function getRandomInt() {
+  const randomNumber = Math.floor(Math.random() * 10) + 1;
+  console.log(randomNumber);
+}
+
+getRandomInt(); // задание 5
+
+function generateRandomArray(maxInteger) {
+  const arrayLength = Math.floor(maxInteger / 2);
+  const randomArray = [];
+
+  for (let i = 0; i < arrayLength; i++) {
+    randomArray.push(Math.floor(Math.random() * (maxInteger + 1)));
+  }
+
+  return randomArray;
+}
+
+const maxNum = 14;
+const randomNumbers = generateRandomArray(maxNum);
+console.log(randomNumbers); // задание 6
+
+function getRandomInteger(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+let randomNumber = getRandomInteger(1, 14);
+console.log(randomNumber);
+
+randomNumber = getRandomInteger(20, 40);
+console.log(randomNumber); // задание 7
+
+let currentDate = new Date();
+console.log(currentDate); // задание 8
+
+let currentDate = new Date();
+currentDate.setDate(currentDate.getDate() + 73);
+console.log(currentDate); // задание 9
+
+function ourDate(date) {
+  const days = [
+    "Воскресенье",
+    "Понедельник",
+    "Вторник",
+    "Среда",
+    "Четверг",
+    "Пятница",
+    "Суббота",
+  ];
+  const months = [
+    "Январь",
+    "Февраль",
+    "Март",
+    "Апрель",
+    "Май",
+    "Июнь",
+    "Июль",
+    "Август",
+    "Сентябрь",
+    "Октябрь",
+    "Ноябрь",
+    "Декабрь",
+  ];
+  const year = date.getFullYear();
+  const month = date.getMonth();
+  const day = date.getDate();
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+  const dayOfWeek = days[date.getDay()];
+
+  return `Дата: ${day} ${months[month]} ${year} - это ${dayOfWeek}. Время: ${hours}:${minutes}:${seconds}`;
+}
+
+console.log(ourDate(new Date())); // задание 10
